f0079a3fc0fbbb91b5d34feda0afb0fc0eb0cf59
Steven Macapagal <sf.macapagal@gmail.com>
2024-03-22 17:04:34 -0500

    adjusted scale_fill_idea, scale_color_idea
    1. added year argument to scale_fill_idea(), scale_color_idea() to match idea_palette_ramp()
    2. documented year argument in scale_color_idea() and added examples

--- R/scales.R
+++ R/scales.R
@@ -10,33 +10,42 @@
 #'
 #' @param discrete whether to use a discrete color palette
 #'
-#' @param ... additional arguments to passed to `scale_color_gradientn`
+#' @parma year Defaults to the most recent branding guidelines (year = 2024), but year = 2019 also available
 #'
+#' @param ... additional arguments to passed to `scale_color_gradientn`
 #'
 #' @importFrom ggplot2 scale_colour_manual
 #'
 #' @examples
 #' library(ggplot2)
+#'
+#' # Explicitly calls the 2024 branding guidelines
 #' ggplot(mtcars, aes(mpg, wt)) +
 #'   geom_point(aes(colour = factor(cyl))) +
-#'   scale_colour_idea(palette="qual")
+#'   scale_colour_idea(palette="qual", year = 2024)
 #'
+#' # Explicitly calls the 2019 branding guidlines
 #' ggplot(mtcars, aes(mpg, wt)) +
 #'   geom_point(aes(colour = hp)) +
-#'   scale_colour_idea(palette="qual", discrete = FALSE)
+#'   scale_colour_idea(palette="qual", discrete = FALSE, year = 2019)
+#'
+#' # Chooses the bluegray palette
 #' ggplot(data = mpg) +
 #'   geom_point(mapping = aes(x = displ, y = hwy, color = class)) +
 #'   scale_colour_idea(palette="bluegray")
-
+#'
 #' @export
 #'
 #' @importFrom ggplot2 discrete_scale scale_color_gradientn
 scale_color_idea <- function(palette="div",
-                             discrete = TRUE, alpha = 1, reverse = FALSE, ...) {
+                             discrete = TRUE,
+                             alpha = 1,
+                             reverse = FALSE,
+                             year = 2024,...) {
   if (discrete) {
-    ggplot2::discrete_scale("colour", "idea", palette=idea_palette_ramp(palette, alpha = alpha, reverse = reverse), ...)
+    ggplot2::discrete_scale("colour", "idea", palette=idea_palette_ramp(palette, alpha = alpha, reverse = reverse, year = year), ...)
   } else {
-    ggplot2::scale_color_gradientn(colours = idea_palette_ramp(palette, alpha = alpha, reverse = reverse, ...)(256))
+    ggplot2::scale_color_gradientn(colours = idea_palette_ramp(palette, alpha = alpha, reverse = reverse, year = year, ...)(256))
   }
 }
 
@@ -67,12 +76,15 @@ scale_colour_idea <- scale_color_idea
 #'
 #' @export
 scale_fill_idea <- function(palette="div",
-                            discrete = TRUE, alpha=1, reverse = TRUE, ...) {
+                            discrete = TRUE,
+                            alpha=1,
+                            reverse = TRUE,
+                            year = 2024, ...) {
   if (discrete) {
-    discrete_scale("fill", "idea", palette=idea_palette_ramp(palette, alpha = alpha, reverse = reverse), ...)
+    discrete_scale("fill", "idea", palette=idea_palette_ramp(palette, alpha = alpha, reverse = reverse, year = year), ...)
   }
   else {
-    scale_fill_gradientn(colours = idea_palette_ramp(palette, alpha = alpha, reverse = reverse)(256), ...)
+    scale_fill_gradientn(colours = idea_palette_ramp(palette, alpha = alpha, reverse = reverse, year = year)(256), ...)
   }
 }
 
